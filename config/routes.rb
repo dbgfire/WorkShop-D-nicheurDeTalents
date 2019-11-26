bb450ed874f3503c94e3f89a9d2f80aa3dc01f24
Alan <alan@pop-os.localdomain>
2019-11-26 15:24:10 +0100

    fix route index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'index/index'
   get 'admin/index'
   get 'static_pages/home'
-  get 'index/index'
+  root 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
