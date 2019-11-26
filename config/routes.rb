c123080c0577de760be659325dd4d6b41c6ba532
Manu <emmanuel.mars@epsi.fr>
2019-11-26 14:52:35 +0100

    add index & css

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'admin/index'
   get 'static_pages/home'
+  get 'index/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
