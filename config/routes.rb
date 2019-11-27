d85f7697a3e073fb16ac1f6bc52d8410dd82d33d
Alan <alan@pop-os.localdomain>
2019-11-27 21:35:46 +0100

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'admin/index'
   get 'static_pages/home'
   root 'index#index'
+
+  resources :users
+  resources :stats
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
