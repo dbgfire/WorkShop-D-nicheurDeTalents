e5effb428bcb14db83a1be2efd3337d37fd36b7e
Meenix <meenix98@gmail.com>
2019-11-27 13:23:17 +0100

    Adding the last user page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'index/index'
   get 'admin/index'
   get 'static_pages/home'
+  get 'final_page/final_page'
   get 'game/game'
   root 'index#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
