74169c1ab3af80b64f3a3c157564ed85eff73e85
Alan <alan@pop-os.localdomain>
2019-11-28 10:07:30 +0100

    add metier for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_083010) do
+ActiveRecord::Schema.define(version: 2019_11_28_085144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_11_28_083010) do
     t.integer "volonte"
     t.integer "ordonne"
     t.integer "logique"
+    t.string "metier"
   end
 
 end
