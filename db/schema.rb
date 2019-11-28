c28bb7f9ba1faa6794ded9f5bd1270f68257fa04
Alan <alan@pop-os.localdomain>
2019-11-28 09:46:27 +0100

    add view rh

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_144317) do
+ActiveRecord::Schema.define(version: 2019_11_28_083010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 2019_11_27_144317) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "patience"
+    t.integer "precision"
+    t.integer "solitaire"
+    t.integer "volonte"
+    t.integer "ordonne"
+    t.integer "logique"
   end
 
 end
